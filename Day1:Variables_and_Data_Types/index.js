689c51b7c35ce10bf116675d2102ec1a88d6bdc5
Rizwan Manzoor <rizwanmanzoor200@gmail.com>
2024-07-18 11:09:34 +0000

    declared and reassigned variables using var, let, and const, explored data types, and observed errors with const.

--- Day1:Variables_and_Data_Types/index.js
+++ Day1:Variables_and_Data_Types/index.js
@@ -1,1 +1,61 @@
-console.log("hello world!");
+let Task1 = "Declare a variable using var, assign it a number, and log the value to the console."
+console.log("Task 1: ", Task1);
+
+var num1 = 10;
+console.log(num1);
+
+
+let Task2 = "Declare a variable using let, assign a string, and log the value to the console."
+console.log("Task 2: ", Task2);
+
+let text1 = "hello world!";
+console.log(text1);
+
+
+let Task3 = "Declare a variable using const, assign a Boolean value, and log the value to the console."
+console.log("Task 3: ", Task3);
+
+const bool1 = true;
+console.log(bool1);
+
+
+let Task4 = "Create variables of different data types (number, string, Boolean, object, array) and log each Variable’s type using the typeof operator."
+console.log("Task 4: ", Task4);
+
+var num = 50;
+console.log(typeof(num));
+
+let str = "javascript";
+console.log(typeof(str));
+
+const bool = false;
+console.log(typeof(bool));
+
+const obj = {
+    0: "Hello",
+    1: "World!"
+}
+console.log(typeof(obj));
+
+const arr = ["Hi", 100, false]
+console.log(typeof(arr));
+
+
+let Task5 = "Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console."
+console.log("Task 5: ", Task5);
+
+let val1 = "hello"
+console.log(val1);
+
+val1 = "world"
+console.log(val1);
+
+
+let Task6 = "Try reassigning a variable declared with const and observe the error."
+console.log("Task 6: ", Task6);
+
+const val2 = "hello, can't reassign a value to const variable!"
+console.log(val2);
+
+// val2 = "world"
+// console.log(val2);
